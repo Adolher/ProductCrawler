3ab342c513e8bafa9070ad624b1d1a8b71962c0b
Adalher <jpw@2711.world>
2023-03-11 06:58:53 +0100

    initial commit

---
+++ ProxyRotator.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 11 06:41:59 2023
+
+@author: jpw
+"""
+import requests
+
+class ProxyRotator():
+    def __init__(self):
+        proxy_list = self.__get_proxy_list()
+        
+        
+    def __get_proxy_list(self):
+        pass
